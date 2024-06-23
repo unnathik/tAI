eca9416a4cc65fd18ff4007290e606a99d5cd848
Adhira Choudhury <achoudhury72@gatech.edu>
2024-06-22 17:06:48 -0700

    Pulled from albert/login

---
+++ tai/src/app/studentLog/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { useRouter } from 'next/navigation'; 
+// import { AnimatePresence, motion } from 'framer-motion';
+
+const StudentLogin = () => {
+    const router = useRouter();
+    return (
+      <div className='h-screen w-full justify-center flex flex-col bg-teal-100'>
+        <div className="h-1/2 flex flex-col px-4 py-4 justify-center">
+          <div className="flex-grow flex px-0 py-0 bg-four rounded-3xl w-1/3 mx-auto shadow-lg bg-white">
+            <p>Student Login</p>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default StudentLogin;
--- tai/src/app/teacherDashboard/page.tsx
+++ tai/src/app/teacherDashboard/page.tsx
@@ -1,3 +1,18 @@
-export default function TeacherDashboard() {
-    return <p>Teacher Dashboard</p>;
-  }
+"use client"
+import { useRouter } from 'next/navigation'; 
+// import { AnimatePresence, motion } from 'framer-motion';
+
+const TeacherDashboard = () => {
+    const router = useRouter();
+    return (
+      <div className='h-screen w-full justify-center flex flex-col bg-teal-100'>
+        <div className="h-1/2 flex flex-col px-4 py-4 justify-center">
+          <div className="flex-grow flex px-0 py-0 bg-four rounded-3xl w-1/3 mx-auto shadow-lg bg-white">
+                <p>Teacher Dashboard</p>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default TeacherDashboard;
