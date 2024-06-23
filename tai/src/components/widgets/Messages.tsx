30b9dc995176dd9a81d8d71c9f274edbd9fd8e2f
Unnathi Kumar <unnathiutpal6@gmail.com>
2024-06-22 18:46:35 -0700

    messaging works

---
+++ tai/src/components/widgets/Messages.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useVoice } from "@humeai/voice-react";
+
+export default function Messages() {
+  const { messages } = useVoice();
+  console.log(messages)
+
+  return (
+    <div>
+      {messages.map((msg, index) => {
+        if (msg.type === "user_message" || msg.type === "assistant_message") {
+          return (
+            <div key={msg.type + index}>
+              <div>{msg.message.role}</div>
+              <div>{msg.message.content}</div>
+            </div>
+          );
+        }
+
+        return null;
+      })}
+    </div>
+  );
+}
